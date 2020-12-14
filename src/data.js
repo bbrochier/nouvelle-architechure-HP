239b8eb3fe0216286d54eef5c30bf54a062594dc
Benoit Brochier <benoit.brochier@gmail.com>
2020-12-14 16:06:53 +0100

    save#1

--- src/data.js
+++ src/data.js
@@ -100,7 +100,75 @@ const dataStoreHP = {
         description: 'string'
       }
     ]
-  }
+  },
+  offreMarchandMaison: {
+    title: 'string',
+    description: 'string',
+    images: {
+      urlSmall: 'string',
+      urlMedium: 'string',
+      urlLarge: 'string',
+      alt: 'string'
+    },
+    button: {
+      target: 'string',
+      label: 'string',
+      url: 'string'
+    },
+    tracking: {
+      refZ: 'string',
+      idc: 'string',
+      category: 'string',
+      action: 'string',
+      label: 'string',
+      other: {
+        track_refZ: 'string' // refZ
+      }
+    },
+    cacheKey: 'string'
+  },
+  reassuranceEcommerce: {
+    title: 'string',
+    items: [
+      {
+        title: 'string', // remplacer les chaines "__SPACE__" par "&nbsp" et "__NL__" par "<br>"
+        image: {
+          url: 'string',
+          alt: ''
+        },
+        url: 'string',
+        target: 'string',
+        tracking: {
+          refZ: 'string',
+          idc: 'string',
+          category: 'string',
+          action: 'string',
+          label: 'string',
+          other: {
+            track_refZ: 'string' // refZ
+          }
+        },
+        cacheKey: 'string'
+      }
+    ]
+  },
+  fildAriane: [
+    {
+      label: 'string',
+      url: 'string',
+      tracking: {
+        refZ: 'string',
+        idc: 'string',
+        category: 'string',
+        action: 'string',
+        label: 'string',
+        other: {
+          track_refZ: 'string' // refZ
+        }
+      },
+      cacheKey: 'string'
+    }
+  ]
 }
 
 export default dataStoreHP
